f3cd780684e59470eaa26f78fa0897c90dfc0776
Eldos <eldos.baktybekov@gmail.com>
2025-02-04 02:28:33 +0600

    :bug: Fix conflict shift dates validation

--- shifts/services/shifts.py
+++ shifts/services/shifts.py
@@ -52,12 +52,15 @@ class ShiftExtraCreateResult:
 
 
 def get_existing_shift_dates(
+        *,
+        staff_id: int,
         expected_dates: Iterable[datetime.date],
 ) -> set[datetime.date]:
     """
     Get existing shift dates from the database.
 
-    Args:
+    Keyword Args:
+        staff_id: staff id.
         expected_dates: existing shifts within these dates.
 
     Returns:
@@ -65,24 +68,34 @@ def get_existing_shift_dates(
     """
     return set(
         Shift.objects
-        .filter(date__in=expected_dates, is_test=False)
+        .filter(
+            staff_id=staff_id,
+            date__in=expected_dates,
+            is_test=False,
+        )
         .values_list('date', flat=True)
     )
 
 
 def validate_conflict_shift_dates(
+        *,
+        staff_id: int,
         expected_dates: Iterable[datetime.date],
 ) -> None:
     """
     Check if there are any conflicts with existing shifts.
 
-    Args:
+    Keyword Args:
+        staff_id: staff id.
         expected_dates: dates of shifts to be created.
 
     Raises:
         ShiftAlreadyExistsError: If shift already exists on any expected date.
     """
-    existing_shift_dates = get_existing_shift_dates(expected_dates)
+    existing_shift_dates = get_existing_shift_dates(
+        staff_id=staff_id,
+        expected_dates=expected_dates,
+    )
     print(existing_shift_dates, set(expected_dates))
     conflict_dates = set(expected_dates).intersection(existing_shift_dates)
     if conflict_dates:
@@ -140,7 +153,7 @@ def create_extra_shift(
     Returns:
         ShiftExtraCreateResult object.
     """
-    validate_conflict_shift_dates([date])
+    validate_conflict_shift_dates(staff_id=staff.id, expected_dates=[date])
 
     shift = Shift(
         staff_id=staff.id,
@@ -176,7 +189,7 @@ def create_regular_shifts(
         ShiftsCreateResult object.
     """
 
-    validate_conflict_shift_dates(dates)
+    validate_conflict_shift_dates(staff_id=staff.id, expected_dates=dates)
 
     shifts_to_create = [Shift(staff=staff, date=date) for date in dates]
     shifts = Shift.objects.bulk_create(shifts_to_create)
