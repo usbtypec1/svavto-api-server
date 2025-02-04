dba40355d5bc662d0e08186e911a2bf44e8c2c8b
Eldos <eldos.baktybekov@gmail.com>
2025-02-04 11:53:37 +0600

    :wrench: Set up config

--- carsharing/settings.py
+++ carsharing/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'carsharing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DATABASE_NAME'),
+        'USER': env.str('DATABASE_USER'),
+        'PASSWORD': env.str('DATABASE_PASSWORD'),
+        'HOST': env.str('DATABASE_HOST'),
+        'PORT': env.int('DATABASE_PORT'),
     }
 }
 
