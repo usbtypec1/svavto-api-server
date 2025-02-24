a96a71cc91d4a42ea43bd98d049017f8ee50198a
Eldos <eldos.baktybekov@gmail.com>
2025-02-24 11:33:14 +0600

    :safety_vest: Add validator

--- shifts/services/shifts/__init__.py
+++ shifts/services/shifts/__init__.py
@@ -11,8 +11,13 @@ from .read import (
     get_staff_ids_with_not_started_shifts_for_today,
 )
 from .reject import mark_shift_as_rejected_now
-from .start import start_shift
-from .validators import ensure_shift_exists, ensure_staff_has_no_active_shift
+from .start import ShiftStartInteractor
+from .validators import (
+    ensure_shift_confirmed,
+    ensure_shift_exists,
+    ensure_staff_has_no_active_shift,
+    ensure_time_to_start_shift,
+)
 
 
 __all__ = (
@@ -30,5 +35,7 @@ __all__ = (
     'get_shifts_by_staff_id',
     'get_current_shift_date',
     'get_staff_ids_with_not_started_shifts_for_today',
-    'start_shift',
+    'ShiftStartInteractor',
+    'ensure_shift_confirmed',
+    'ensure_time_to_start_shift',
 )
--- shifts/services/shifts/validators.py
+++ shifts/services/shifts/validators.py
@@ -1,4 +1,12 @@
-from shifts.exceptions import ShiftNotFoundError, StaffHasActiveShiftError
+import datetime
+
+from django.utils import timezone
+
+from core.services import get_current_shift_date
+from shifts.exceptions import (
+    InvalidTimeToStartShiftError, ShiftNotConfirmedError, ShiftNotFoundError,
+    StaffHasActiveShiftError,
+)
 from shifts.models import Shift
 
 
@@ -14,3 +22,28 @@ def ensure_staff_has_no_active_shift(staff_id: int) -> None:
 def ensure_shift_exists(shift_id: int) -> None:
     if not Shift.objects.filter(id=shift_id).exists():
         raise ShiftNotFoundError
+
+
+def ensure_shift_confirmed(shift: Shift) -> None:
+    if shift.confirmed_at is not None:
+        raise ShiftNotConfirmedError
+
+
+def ensure_time_to_start_shift() -> None:
+    now = timezone.now()
+
+    shift_date = get_current_shift_date()
+    start_time = datetime.datetime.combine(
+        shift_date,
+        datetime.time(18, 30),
+        tzinfo=datetime.UTC,
+    )
+    next_day = start_time + datetime.timedelta(days=1)
+    next_day_noon = next_day.replace(
+        hour=9,
+        minute=0,
+        second=0,
+        microsecond=0,
+    )
+    if not (start_time <= now <= next_day_noon):
+        raise InvalidTimeToStartShiftError
