3a1b71e1b1476311bb7600b6b44885c170aa9778
Eldos <eldos.baktybekov@gmail.com>
2025-04-23 14:04:44 +0600

    ✨ Add management command to create weekend bonuses based on shift data

---
+++ shifts/management/commands/create_weekend_bonuses.py
@@ -0,0 +1,27 @@
+import datetime
+
+from django.core.management import BaseCommand
+from django.db.models import Count
+
+from shifts.models import Shift
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'date', type=datetime.date.fromisoformat,
+            help='ISOformat - YYYY-MM-DD',
+        )
+
+    def handle(self, *args, **options):
+        date: datetime.date = options['date']
+        values = (
+            Shift.objects
+            .filter(date=date, is_test=False, is_extra=False)
+            .annotate(cars_count=Count('cars'))
+            .filter(cars_count__gte=4)
+            .values('cars_count', 'staff_id')
+        )
+        for value in values:
+            self.stdout.write(value['staff_id'], value['cars_count'])
