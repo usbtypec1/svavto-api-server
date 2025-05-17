a95e88a96b33bc1808ce43193243bdd912b16036
Eldos <eldos.baktybekov@gmail.com>
2025-05-17 23:38:04 +0600

    :white_check_mark: Refactor CarWashListOutputSerializer to use explicit fields and improve structure

--- car_washes/serializers.py
+++ car_washes/serializers.py
@@ -27,21 +27,29 @@ class CarWashListInputSerializer(serializers.Serializer):
     include_hidden = serializers.BooleanField(default=True)
 
 
-class CarWashListOutputSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CarWash
-        fields = (
-            "id",
-            "name",
-            "comfort_class_car_washing_price",
-            "business_class_car_washing_price",
-            "van_washing_price",
-            "windshield_washer_price_per_bottle",
-            "is_hidden",
-            "created_at",
-            "updated_at",
-        )
-
+class CarWashListOutputSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    car_transporters_comfort_class_car_washing_price = (
+        serializers.IntegerField(source="comfort_class_car_washing_price")
+    )
+    car_transporters_business_class_car_washing_price = (
+        serializers.IntegerField(source="business_class_car_washing_price")
+    )
+    car_transporters_van_washing_price = serializers.IntegerField(
+        source="van_washing_price",
+    )
+    car_transporters_and_washers_comfort_class_price = (
+        serializers.IntegerField()
+    )
+    car_transporters_and_washers_business_class_price = (
+        serializers.IntegerField()
+    )
+    car_transporters_and_washers_van_price = serializers.IntegerField()
+    windshield_washer_price_per_bottle = serializers.IntegerField()
+    is_hidden = serializers.BooleanField()
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
 
 class CarWashRetrieveOutputSerializer(serializers.ModelSerializer):
     class Meta:
