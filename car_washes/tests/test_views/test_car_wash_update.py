cb7b58d5767b5168c49b586503df665e5ff7b687
Eldos <eldos.baktybekov@gmail.com>
2025-05-18 12:41:06 +0600

    :white_check_mark: Update CarWashUpdateInputSerializer to include detailed pricing fields and set default values

--- car_washes/tests/test_views/test_car_wash_update.py
+++ car_washes/tests/test_views/test_car_wash_update.py
@@ -9,8 +9,19 @@ from django.utils.translation import gettext as _
 def test_car_wash_not_found():
     client = APIClient()
     url = reverse('car-washes:detail-update-delete', args=(53443,))
+    data = {
+        "name": "Updated Car Wash",
+        "car_transporters_comfort_class_car_washing_price": 100,
+        "car_transporters_business_class_car_washing_price": 200,
+        "car_transporters_van_washing_price": 300,
+        "car_transporters_and_washers_comfort_class_price": 400,
+        "car_transporters_and_washers_business_class_price": 500,
+        "car_transporters_and_washers_van_price": 600,
+        "windshield_washer_price_per_bottle": 700,
+        "is_hidden": False,
+    }
 
-    response = client.put(url)
+    response = client.put(url, data, format='json')
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.data == {
